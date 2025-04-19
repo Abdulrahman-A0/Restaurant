5e67ab512cf1ca4aaed7f0015071607f808a7752
Ali Mohamed <aliabubeah832@gmail.com>
2025-04-19 17:10:57 +0200

    Added Menu View

---
+++ Resturant_Project/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resturant_Project.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View("Menu");
+        }
+    }
+}
