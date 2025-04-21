b0563a347f03df41afc45e9d9783f89dd28bf29a
Abdulrahman <abdelrhmanelhshamy@gmail.com>
2025-04-21 23:06:37 +0200

    Handled Login&Register

---
+++ Resturant_Project/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant_Project.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Emial")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter Your Password")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
+        public string Password { get; set; }
+    }
+}
