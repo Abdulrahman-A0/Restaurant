1a20c9fef1e7972b50a4574d5f8ea22c9c5e5747
Ali Mohamed <aliabubeah832@gmail.com>
2025-04-21 19:31:06 +0200

    Added Dashboard

---
+++ Resturant_Project/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Category()
+        {
+            return View();
+        }
+        public IActionResult Dishes()
+        {
+            return View();
+        }
+    }
+}
