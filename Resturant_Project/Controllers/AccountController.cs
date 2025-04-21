2dca6a362a79924413aa6378b99faa7fa3be077f
Ali Mohamed <aliabubeah832@gmail.com>
2025-04-21 23:04:18 +0200

    Added Forget,Verifry views

---
+++ Resturant_Project/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
