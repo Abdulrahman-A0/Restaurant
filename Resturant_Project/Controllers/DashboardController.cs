0bbbee885346cc59ed881469465ecabf948ccaed
Abdulrahman <abdelrhmanelhshamy@gmail.com>
2025-04-21 21:07:44 +0200

    Linked Dashboard With Database

--- Resturant_Project/Controllers/DashboardController.cs
+++ Resturant_Project/Controllers/DashboardController.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_Project.Data;
 
 namespace Restaurant_Project.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Category()
         {
-            return View();
+            var categoryModel = _context.Categories.ToList();
+            return View(categoryModel);
         }
         public IActionResult Dishes()
         {
-            return View();
+            var dishesModel = _context.Dishes.ToList();
+            return View(dishesModel);
         }
     }
 }
