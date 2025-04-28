94c5014ad05f3a3e34e494ae7a3f789fbefbd066
Ali Mohamed <aliabubeah832@gmail.com>
2025-04-28 18:56:49 +0300

    added search view

---
+++ Resturant_Project/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Project.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
