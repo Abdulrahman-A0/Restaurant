8aa64d8e42a1374b2caa5886a17f16b9fb44df24
Abdulrahman <abdelrhmanelhshamy@gmail.com>
2025-04-28 20:15:36 +0300

    Added Search Logic

--- Resturant_Project/Controllers/SearchController.cs
+++ Resturant_Project/Controllers/SearchController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Restaurant_Project.Data;
 
 namespace Restaurant_Project.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Search()
+        private readonly AppDbContext _context;
+
+        public SearchController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Search(string keyword)
+        {
+            var result = _context.Dishes
+                .Where(x => x.Name.Contains(keyword)).ToList();
+            return View(result);
         }
     }
 }
